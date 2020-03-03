98861fe877ae8ba9db51a010161ff14219d6310d
Ryan Nicholson <rbnich2012@gmail.com>
2020-03-03 01:44:19 -0500

    update

---
+++ 24Hour.Services/CommentService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class CommentService
+    {
+        public IEnumerable<CommentListItem> GetComments()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                         .Comment
+                         .Where(e => e.Author == _userId)
+                         .Select(
+                            e =>
+                                new CommentsListItem
+                                {
+                                    Arthor = e.Arthor,
+                                    Text = e.Text,
+                                    CommentPost = e.CommentPost,
+                                }
+                        );
+                return query.ToArray();
+
+            }
+        }
+        public CommentDetail GetCommentById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Comment
+                        .Single(e => e.Arthor == id && e.Authur == _userId);
+                return
+                    new CommentDetail
+                    {
+                        Arthor = entity.Arthor,
+                        Text = entity.Text,
+                        CommentPost = entity.CommentPost,
+
+                    };
+
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CommentController.cs
@@ -0,0 +1,41 @@
+﻿using _24Hour.Services;
+using _24Hours.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TwentyFourHour.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class CommentController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            CommentService commentService = CreateCommentService();
+            var comment = CommentService.GetComments();
+            return Ok(comment);
+        }
+
+        public IHttpActionResult Post(CommentCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCommentService();
+
+            if (!service.CommentCreate(Comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private CommentService CreateCommentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(userId);
+            return commentService;
+        }
+    }
+}
